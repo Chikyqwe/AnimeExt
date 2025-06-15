15fb10cdb93514d19deba40ad819fb338d9c2968
chikiyinyang <124471620+Chikyqwe@users.noreply.github.com>
2025-06-14 19:20:37 -0600

    14 / jun / 2025

--- main.js
+++ main.js
@@ -74,13 +74,13 @@ app.get('/api/player', (req, res) => {
 
 // =================== API JSONS ===================
 app.get('/json-list', (req, res) => {
-  const files = fs.readdirSync("/").filter(f => f.endsWith('.json'));
+  const files = fs.readdirSync(JSON_FOLDER).filter(f => f.endsWith('.json'));
   res.json(files);
 });
 
 app.get('/jsons/:filename', (req, res) => {
   const filename = req.params.filename;
-  res.sendFile(path.join(filename));
+  res.sendFile(path.join(JSON_FOLDER, filename));
 });
 
 // =================== PROXY DE IMÁGENES ===================
