8175481e565393fa57b692f91e1d5095e78ae2f9
chikiyinyang <124471620+Chikyqwe@users.noreply.github.com>
2025-07-03 17:28:30 -0600

    Update worker-mantenimiento.js

--- worker-mantenimiento.js
+++ worker-mantenimiento.js
@@ -1,9 +1,11 @@
 const { parentPort } = require('worker_threads');
-const { main } = require('./anim');
+const { main } = require('./anim'); // Asegúrate que este nombre coincida
 
 (async () => {
   try {
-    await main({ log: msg => parentPort.postMessage({ type: 'log', msg }) });
+    await main({
+      log: msg => parentPort.postMessage({ type: 'log', msg })
+    });
     parentPort.postMessage({ type: 'done' });
   } catch (err) {
     parentPort.postMessage({ type: 'error', err: err.message || String(err) });
