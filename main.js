713c923ae28c918dcf025dabbbbabfad1890fc46
chikiyinyang <124471620+Chikyqwe@users.noreply.github.com>
2025-08-14 12:47:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = 2021;
 
 app.get('/', async (req, res) => {
   const url = req.query.url;
