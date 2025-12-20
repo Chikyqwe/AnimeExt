4583ad677cd48b970c25e932a36f55a378fac190
chikiyinyang <124471620+Chikyqwe@users.noreply.github.com>
2025-12-19 20:09:03 -0500

    Actualizar index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,8 @@ const animeController = require('../controllers/animeController');
 const imageController = require('../controllers/imageController');
 
 router.get('/anime/list', animeController.list);
+router.get('/anime/list/ext/beta/cordova/beta/anime/app/chikyqwe', animeController.list);
+
 router.get('/image', imageController.imageProxy);
 router.get('/anime/last', animeController.last);
 
