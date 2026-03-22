f6493b0dee150a2302bbed64a6928f0e9de39974
chikiyinyang <124471620+Chikyqwe@users.noreply.github.com>
2026-03-21 22:55:35 -0500

    env

--- src/services/supabase/supabase.js
+++ src/services/supabase/supabase.js
@@ -1,8 +1,7 @@
 require('dotenv').config({ quiet: true });
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://fpltgmpbdukudlnjfqcf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwbHRnbXBiZHVrdWRsbmpmcWNmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3Mzk1NDQyMywiZXhwIjoyMDg5NTMwNDIzfQ.gV-l-Y08CuJmcJ2pI0Br3T30h04zCz6pO5TMZAoR0P8'; // Usa service_role para control total
-
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL; 
+const supabaseKey = process.env.NEXT_PRIVATE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 module.exports = { supabase };
